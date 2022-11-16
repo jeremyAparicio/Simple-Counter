c5051eb734bf0e1319a9639c14225a11323abca5
jeremyAparicio <jeremy.aparicio.g@gmail.com>
2022-11-16 19:41:58 +0000

    Se realizo contador incremental

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,22 +3,27 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
-const Home = () => {
+const Home = ({segundos}) => {
+	let string = segundos.toString();
+	let unidad = string[string.length -1]
+	let decena = string[string.length -2]
+	let centena = string[string.length -3]
+	let milesima = string[string.length -4]
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="texto">
+			<span>Contador Incremental</span>
+			</div>
+			<div className="textobtn">
+			<button className="btn"><span>🕒</span></button>
+			<button className="btn"><span>{milesima == undefined? "0": milesima}</span></button>
+			<button className="btn"><span>{centena == undefined? "0": centena}</span></button>
+			<button className="btn"><span>{decena == undefined? "0": decena}</span></button>
+			<button className="btn"><span>{unidad == undefined? "0": unidad}</span></button>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,17 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+
 //import your own components
 import Home from "./component/home.jsx";
 
+let tiempo = 0;
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(()=>{
+    console.log(tiempo);
+    ReactDOM.render(<Home segundos={tiempo}/>, document.querySelector("#app"));
+    tiempo ++;
+},1000)
+
+
+
